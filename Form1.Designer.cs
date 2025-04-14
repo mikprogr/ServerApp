7c71d3862aa86b675ec7fd4a2d48c647e0803496
mikprogr <157276621+mikprogr@users.noreply.github.com>
2025-04-14 10:40:52 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ServerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 50);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 246);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(409, 50);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(198, 246);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 299);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Wiadomości odebrane";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(430, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Wiadomości do wysłania";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Server start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(637, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Wyśllij";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(284, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Server stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(67, 398);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(540, 22);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(281, 433);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Server status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 475);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "ServerApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Authentication;
+using System.Text;
+using System.IO;
+using System.Net.Security;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener listener;
+        private TcpClient activeClient;
+        private SslStream sslStream;
+        private bool serverRunning = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (serverRunning)
+            {
+                MessageBox.Show("Server is already running");
+                return;
+            }
+
+            Task.Run(() => StartServer());
+        }
+
+        private void StartServer()
+        {
+            try
+            {
+                var certificate = new X509Certificate2(@"C:\Users\Mikołaj\Desktop\ServerApp\server.pfx", "password");
+
+                listener = new TcpListener(IPAddress.Any, 12345);
+                listener.Start();
+                serverRunning = true;
+
+                this.Invoke(new Action(() =>
+                {
+                    if (!string.IsNullOrWhiteSpace(textBox3.Text))
+                    {
+                        textBox3.Clear();
+                    }
+                    textBox3.AppendText("Server is listening..." + Environment.NewLine);
+                }));
+
+                while (serverRunning)
+                {
+                    activeClient = listener.AcceptTcpClient();
+                    sslStream = new SslStream(activeClient.GetStream(), false);
+
+                    try
+                    {
+                        sslStream.AuthenticateAsServer(certificate, false, SslProtocols.Tls12, true);
+
+                        this.Invoke(new Action(() =>
+                        {
+                            textBox3.AppendText("Client connected and SSL/TLS authentication succeeded" + Environment.NewLine);
+                        }));
+
+                        // Obsługuje komunikację z klientem w osobnym wątku
+                        Task.Run(() => HandleClientCommunication());
+                    }
+                    catch (Exception ex)
+                    {
+                        this.Invoke(new Action(() =>
+                        {
+                            textBox3.AppendText("Error: " + ex.Message + Environment.NewLine);
+                        }));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    textBox3.AppendText("Error starting server: " + ex.Message + Environment.NewLine);
+                }));
+            }
+        }
+
+        private void HandleClientCommunication()
+        {
+            try
+            {
+                if (sslStream.CanRead)
+                {
+                    using (var reader = new StreamReader(sslStream, Encoding.UTF8))
+                    {
+                        string message;
+                        while ((message = reader.ReadLine()) != null)
+                        {
+                            this.Invoke(new Action(() =>
+                            {
+                                textBox1.AppendText("Received message: " + message + Environment.NewLine);
+                            }));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    textBox3.AppendText("Error reading from client: " + ex.Message + Environment.NewLine);
+                }));
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!serverRunning)
+            {
+                textBox3.AppendText("Server is not running" + Environment.NewLine);
+                return;
+            }
+
+            try
+            {
+                serverRunning = false;
+
+                if (listener != null)
+                {
+                    listener.Stop();
+                }
+
+                this.Invoke(new Action(() =>
+                {
+                    if (!string.IsNullOrWhiteSpace(textBox3.Text))
+                    {
+                        textBox3.Clear();
+                    }
+                    textBox3.AppendText("Server stopped" + Environment.NewLine);
+                }));
+            }
+            catch (Exception ex)
+            {
+                textBox3.AppendText("Error stopping server: " + ex.Message + Environment.NewLine);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (activeClient == null || !activeClient.Connected)
+            {
+                textBox3.AppendText("No active client or client is disconnected." + Environment.NewLine);
+                return;
+            }
+
+            string messageToSend = textBox2.Text;
+            if (string.IsNullOrWhiteSpace(messageToSend))
+            {
+                textBox3.AppendText("Please enter a message to send." + Environment.NewLine);
+                return;
+            }
+
+            try
+            {
+                // Używamy przechowywanego SslStream do wysyłania wiadomości
+                if (sslStream != null && sslStream.CanWrite)
+                {
+                    using (var writer = new StreamWriter(sslStream, Encoding.UTF8, 1024, leaveOpen: true))
+                    {
+                        writer.WriteLine(messageToSend);
+                        writer.Flush();
+                        textBox3.AppendText("Message sent: " + messageToSend + Environment.NewLine);
+                    }
+                }
+                else
+                {
+                    textBox3.AppendText("SSL Stream is not available for writing." + Environment.NewLine);
+                }
+            }
+            catch (Exception ex)
+            {
+                textBox3.AppendText("Error sending message: " + ex.Message + Environment.NewLine);
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
